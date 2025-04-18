fba232edb253c3ab32d107ad2e0573383d24e2b6
Kevin Chavez <mr.kev@me.com>
2025-04-17 19:17:17 -0600

    fix build for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [
+    // for fast refresh, the automatic JSX runtime, etc
+    react(),
+  ],
   build: {
     outDir: "docs",
   },
+  // instead of having absolute paths pointing at assets in `index.html`, use
+  // relative paths. Works better with github pages where /assets/foobar.js
+  // referes to another site
+  base: "./",
 });
