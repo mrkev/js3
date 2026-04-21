e0bfdd04cbd40abe3fdcd934207d9154ff90c313
Kevin Chavez <mr.kev@me.com>
2026-04-21 06:41:50 -0700

    upgrade eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,88 +1,43 @@
-import { fixupConfigRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
+import react from "eslint-plugin-react";
 import reactCompiler from "eslint-plugin-react-compiler";
+import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: eslint.configs.recommended,
-  allConfig: eslint.configs.all,
-});
-
 export default tseslint.config(
+  { ignores: ["build", "components/**", "docs"] },
   eslint.configs.recommended,
-  reactCompiler.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    ignores: ["build", "components/**", "docs"],
-  },
-  ...fixupConfigRules(
-    compat.extends("plugin:react-hooks/recommended", "plugin:react/recommended", "plugin:react/jsx-runtime"),
-  ),
+  react.configs.flat.recommended,
+  react.configs.flat["jsx-runtime"],
+  reactHooks.configs.flat.recommended,
+  reactCompiler.configs.recommended,
   {
     plugins: {
       "react-refresh": reactRefresh,
     },
     languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-
+      globals: globals.browser,
       ecmaVersion: "latest",
       sourceType: "module",
     },
-
     settings: {
-      react: {
-        version: "detect",
-      },
+      react: { version: "detect" },
     },
-
     rules: {
-      "@typescript-eslint/no-empty-function": [
-        "off",
-        {
-          allow: ["private-constructors"],
-        },
-      ],
-
-      "@typescript-eslint/ban-types": "off",
-      "no-useless-escape": "off",
-      indent: "off",
-      quotes: "off",
-      "no-unused-vars": "off",
-
-      "@typescript-eslint/no-unused-vars": [
-        "off",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-        },
-      ],
-      "typescript-eslint/no-empty-function": "off",
-      "no-new-func": "warn",
+      "@typescript-eslint/no-empty-function": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/ban-ts-ignore": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
-      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
       "no-case-declarations": "off",
+      "no-new-func": "warn",
+      "no-unused-vars": "off",
+      "no-useless-escape": "off",
       "react/no-children-prop": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        {
-          allowConstantExport: true,
-        },
-      ],
+      "react/no-unescaped-entities": "off",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
   },
 );
